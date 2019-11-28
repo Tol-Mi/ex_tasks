41d92b7dd68b1f84f5a48e97b716bc0d09873a8a
Mikhail Danilov <mfdanilovv@gmail.com>
2019-11-28 14:11:11 +0300

    4.1

---
+++ 4-Поиски/4.1/index.js
@@ -0,0 +1,13 @@
+function linearSearch(array, element) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] == element) {
+            return 'Элемент найден на ' + i + ' позиции массива';
+        }
+    }
+    return 'Элемент не найден';
+}
+
+let array = ['1', '2', '3', '4', '5'];
+let element = 3;
+
+alert(linearSearch(array, element));
